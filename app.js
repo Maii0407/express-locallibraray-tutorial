6ceab9dab05b96072790bf8c979afd4ed96b3db7
Akmal Izuddin <izuddin0407@gmail.com>
2022-06-08 17:28:03 +0800

    ready for deployment

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require( './routes/catalog' );
 
+var compression = require( 'compression' );
+var helmet = require( 'helmet' );
+
 var app = express();
+
+app.use( helmet());
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
@@ -26,6 +31,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use( compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
